eb2c3a2b0a788fa0ce766ca1ced39a163e5451b0
bkhezry <bkhezry@gmail.com>
2016-12-05 19:31:39 +0330

    change to TYPE_CLASS_NUMBER keyboard type.

--- maskformatter/src/main/java/com/azimolabs/maskformatter/CharInputType.java
+++ maskformatter/src/main/java/com/azimolabs/maskformatter/CharInputType.java
@@ -11,17 +11,7 @@ public class CharInputType {
         if (Character.isDigit(nextChar)) {
             return InputType.TYPE_CLASS_NUMBER;
         }
-
-        switch (nextChar) {
-            case 'd':
-                return InputType.TYPE_CLASS_NUMBER;
-            case 'A':
-            case 'Z':
-            case '%':
-                return InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS;
-        }
-
-        return InputType.TYPE_CLASS_TEXT;
+        return InputType.TYPE_CLASS_NUMBER;
     }
 
 }
